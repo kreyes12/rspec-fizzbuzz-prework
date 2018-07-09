1b63404d7c567f98bb4fa45cd4dd11295d9c1820
Karla R <karla.y.reyes12@gmail.com>
2018-07-09 07:05:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(integer)
-  if integer % 15 == 0
+  if (integer % 3 == 0 && integer % 5 == 0)
+    return "FizzBuzz"
+  elsif integer % 3 == 0
     return "Fizz"
   elsif integer % 5 == 0
     return "Buzz"
-  elsif integer >= 15 && integer % 15 == 0
-    return "FizzBuzz"
 end
   else 
     return nil
